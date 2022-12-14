ce756bc497d8f2d3712b7a0dc4ff53af994b56d3
Pepa Popova <pepi.popova93@gmail.com>
2022-12-14 23:53:09 +0200

    New functionality for staff users added to add destinations

--- travel_frame/destinations/views.py
+++ travel_frame/destinations/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from travel_frame.destinations.forms import AddCountriesForm
+
+
+@login_required
+def add_destination(request):
+
+    if not request.user.is_staff:
+        return redirect('home page')
+
+    if request.method == 'GET':
+        form = AddCountriesForm()
+    else:
+        form = AddCountriesForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home page')
+
+    context = {
+        'form': form,
+    }
+
+    return render(
+        request,
+        'destinations/add_destination.html',
+        context,
+    )
